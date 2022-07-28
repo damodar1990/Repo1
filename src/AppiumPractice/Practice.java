a550bb9c8d18955fbb35f8807e821ca4aa193c5c
damodar <damusept12@gmail.com>
2022-07-28 01:42:14 -0700

    hi my first commit

---
+++ src/AppiumPractice/Practice.java
@@ -0,0 +1,24 @@
+package AppiumPractice;
+
+import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import io.appium.java_client.android.AndroidDriver;
+
+public class Practice 
+{
+	//WebDriver driver=new ChromeDriver();
+	
+	DesiredCapabilities capabilities=new DesiredCapabilities();
+	
+	capabilities.
+	
+	AndroidDriver mobiledriver=new AndroidDriver(capabilities);
+	
+	
+
+
+	
+}
